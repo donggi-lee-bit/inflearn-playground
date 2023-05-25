29a5cccab2f562fa42e23e15bd83fba8249ede74
donggi-lee-bit <devdonggilee@gmail.com>
2023-05-25 14:03:16 +0900

    토비스프링부트 refactor: 공통적으로 처리해야하는 로직을 프론트 컨트롤러에서 처리하도록 변경
    - 서블릿에서 처리해야하는 공통적인 로직이 존재
    - 중복되는 로직은 프론트 컨트롤러에서 처리하고 각기 다른 요청은 서블릿에게 위임

--- toby-springboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ toby-springboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -12,22 +12,34 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 
 public class HellobootApplication {
 
 	public static void main(String[] args) {
 		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
 		WebServer webServer = serverFactory.getWebServer(servletContext -> {
-			servletContext.addServlet("hello", new HttpServlet() {
+			HelloController helloController = new HelloController();
+
+			servletContext.addServlet("frontcontroller", new HttpServlet() {
 				@Override
 				protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-					String name = req.getParameter("name");
+					if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
+						String name = req.getParameter("name");
 
-					resp.setStatus(OK.value());
-					resp.setHeader(CONTENT_TYPE, TEXT_PLAIN_VALUE);
-					resp.getWriter().println("Hello " + name);
+						String ret = helloController.hello(name);
+
+						resp.setStatus(OK.value());
+						resp.setHeader(CONTENT_TYPE, TEXT_PLAIN_VALUE);
+						resp.getWriter().println(ret);
+					} else if (req.getRequestURI().equals("/user")) {
+
+					} else {
+						resp.setStatus(HttpStatus.NOT_FOUND.value());
+					}
 				}
-			}).addMapping("/hello");
+			}).addMapping("/*");
 		});
 		webServer.start();
 	}
