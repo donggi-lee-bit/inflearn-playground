3ef6c7441da9afe56b5dace46e9cffe799bad47c
donggi-lee-bit <devdonggilee@gmail.com>
2023-05-25 13:45:15 +0900

    feat: 서블릿 컨테이너 생성
    - @SpringBootApplication 어노테이션 제거 후 서블릿 컨테이너를 생성하여 서버처럼 동작하도록 로직 추가
    - 서블릿 컨테이너로 `/hello` 라는 요청이 왔을 때 처리할 수 있도록 서블릿 매핑
    - 요청의 응답 코드, 헤더, 바디 생성
      - 응답 코드, 헤더의 Content-Type과 같은 데이터를 입력할 때 휴먼 에러를 줄이기 위해 Spring 에서 제공하는 enum 사용을 권장
    - 쿼리 스트링으로 파라미터를 받아 동적으로 처리

--- toby-springboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ toby-springboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -1,13 +1,35 @@
 package tobyspring.helloboot;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
+import static org.springframework.http.HttpStatus.OK;
+import static org.springframework.http.MediaType.TEXT_PLAIN_VALUE;
 
-@SpringBootApplication
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+
 public class HellobootApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HellobootApplication.class, args);
+		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+		WebServer webServer = serverFactory.getWebServer(servletContext -> {
+			servletContext.addServlet("hello", new HttpServlet() {
+				@Override
+				protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+					String name = req.getParameter("name");
+
+					resp.setStatus(OK.value());
+					resp.setHeader(CONTENT_TYPE, TEXT_PLAIN_VALUE);
+					resp.getWriter().println("Hello " + name);
+				}
+			}).addMapping("/hello");
+		});
+		webServer.start();
 	}
 
 }
