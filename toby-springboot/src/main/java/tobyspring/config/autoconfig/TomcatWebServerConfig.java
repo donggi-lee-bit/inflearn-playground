88a112cedb22359a706f97874c2de089f9aead88
donggi-lee-bit <devdonggilee@gmail.com>
2023-06-06 20:17:12 +0900

    토비스프링부트 refactor: 스프링 부트 스타일로 프로퍼티 클래스 @Import 할 때 목적을 알 수 있도록
    - 어렵다 어려워!!!!

--- toby-springboot/src/main/java/tobyspring/config/autoconfig/TomcatWebServerConfig.java
+++ toby-springboot/src/main/java/tobyspring/config/autoconfig/TomcatWebServerConfig.java
@@ -4,21 +4,23 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Import;
 import tobyspring.config.ConditionalMyOnClass;
+import tobyspring.config.EnableMyConfigurationProperties;
 import tobyspring.config.MyAutoConfiguration;
 
 @MyAutoConfiguration
 @ConditionalMyOnClass("org.apache.catalina.startup.Tomcat")
-@Import(ServerProperties.class)
+@EnableMyConfigurationProperties(ServerProperties.class)
 public class TomcatWebServerConfig {
 
     @Bean("tomcatWebServerFactory")
     @ConditionalOnMissingBean   // 사용자가 등록한 빈이 없을 때 해당 빈을 등록
     public ServletWebServerFactory serverFactory(ServerProperties serverProperties) {
         TomcatServletWebServerFactory tomcatServletWebServerFactory = new TomcatServletWebServerFactory();
+
         tomcatServletWebServerFactory.setContextPath(serverProperties.getContextPath());
         tomcatServletWebServerFactory.setPort(serverProperties.getPort());
+        
         return tomcatServletWebServerFactory;
     }
 }
